47b55c99ee540c141c99f5a45a0a8d4ef29a81d7
Artyom <loiko.artyom@gmail.com>
2023-06-25 23:08:42 +0300

    add compress & decompress

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,11 +3,15 @@ import {cd} from './cd.js'
 import {ls} from './ls.js'
 import {hash} from './hash.js'
 import {os} from './os.js'
+import {compress} from './compress.js'
+import {decompress} from './decompress.js'
 
 export const COMMANDS = {
   up,
   cd,
   ls,
   os,
-  hash
+  hash,
+  compress,
+  decompress
 }
