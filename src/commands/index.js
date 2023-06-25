38beeeac36b341b9e5351fd0f1a2eceddda6630a
Artyom <loiko.artyom@gmail.com>
2023-06-25 23:23:57 +0300

    add cat operation

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,7 @@
 import {up} from './up.js'
 import {cd} from './cd.js'
 import {ls} from './ls.js'
+import {cat} from './cat.js'
 import {hash} from './hash.js'
 import {os} from './os.js'
 import {compress} from './compress.js'
@@ -10,6 +11,7 @@ export const COMMANDS = {
   up,
   cd,
   ls,
+  cat,
   os,
   hash,
   compress,
