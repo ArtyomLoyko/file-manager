925f11c0e1aeb53d7e3c895c6952bf23f38bebdb
Artyom <loiko.artyom@gmail.com>
2023-06-25 18:51:55 +0300

    add hash operation

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,9 +1,11 @@
 import {up} from './up.js'
 import {cd} from './cd.js'
 import {ls} from './ls.js'
+import {hash} from './hash.js'
 
 export const COMMANDS = {
   up,
   cd,
-  ls
+  ls,
+  hash
 }
