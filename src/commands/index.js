ea6b667055090ed208d972aa7b71cd443f5fb11a
Artyom <loiko.artyom@gmail.com>
2023-06-25 15:43:47 +0300

    add: up operation

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,3 +1,5 @@
+import {up} from './up.js'
+
 export const COMMANDS = {
-  'log': (...args) => console.log(args.slice(0, -1).join(' '))
+  up
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as readline from 'node:readline/promises'
 import os from 'os'
 import { COMMANDS } from './commands/index.js'
+import { Path } from './utils/path.js'
 
 const rl =  readline.createInterface({
   input: process.stdin,
@@ -9,8 +10,9 @@ const rl =  readline.createInterface({
 
 const username = process.argv.find(arg => arg.startsWith('--username')).substring(11)
 console.log(`Welcome to the File Manager, ${username}!`)
-let path = os.homedir()
-console.log(`You are currently in ${path}`)
+
+let currentPath = new Path(os.homedir())
+currentPath.log()
 
 rl.on('line', (input) => {
   if (input === '.exit') {
@@ -22,7 +24,8 @@ rl.on('line', (input) => {
 
   if (commandHandler) {
     try {
-      commandHandler(...restArgs, path)
+      commandHandler(...restArgs, currentPath)
+      currentPath.log()
     } catch {
       console.log('Operation failed')
     }
